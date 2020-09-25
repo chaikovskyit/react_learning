17d88c320b2582b3799beeeb7be5ceefef722265
chaikovskyit <chaikovskyit@gmail.com>
2020-09-25 22:40:07 +0300

    create first component

---
+++ src/Car/Car.js
@@ -0,0 +1,35 @@
+import React from 'react'
+// VARIANT 1
+// function car(){
+//     return(
+//         <h2>This is car component</h2>                        
+//     )
+// }
+
+// VARIANT 2
+// const car = () => {
+//     return (                                                  
+//         <div>This is car component</div>
+//     )
+// }
+
+
+// VARIANT 3
+// // const car = () => (
+// //     <div>
+// //         This is car component
+// //         <strong>test</strong>
+// //     </div>
+// // )
+
+
+// export default car
+
+
+//ultra small code 
+export default () => (
+    <div>
+        This is car component
+        <strong>Test</strong>
+    </div>
+)
