9acdc60472271f3516411cd94217a3014a1d8a15
chaikovskyit <chaikovskyit@gmail.com>
2020-09-29 19:05:41 +0300

    add new component

--- src/App.js
+++ src/App.js
@@ -1,28 +1,19 @@
 import React from 'react';
 import './App.css';
-import Car from './Car/Car'
+import Header from './Header.js';
+import Technologies from './Technologies';
 
-function App() {
-  state = {
-    cars: [
-      {name: 'Ford', year: '2018'},
-      {name: 'Skoda', year: '2012'},
-      {name: 'Tesla', year: '2010'}
-    ]
-  }
-  const divStyle = {
-    textAlign: 'center'
-  }
-  return (
-    <div style={divStyle}>
-        <h1>Hello world!!!</h1>
-
-        <Car name={'Skoda'} year={2007} />
-        <Car name={'Ford'} year={2008} />
-        <Car name={'Tesla'} year={2013} />
-
-    </div>
-  );
+const App = () => {
+    return (
+      <div>
+        <Header />
+        <Technologies />
+      </div>
+    );
 }
+
+
+
+
 
 export default App;
