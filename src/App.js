69ab16aba147c005d3a075a52b0847b8e546701f
chaikovskyit <chaikovskyit@gmail.com>
2020-10-01 13:46:30 +0300

    add new component Dialogs

--- src/App.js
+++ src/App.js
@@ -1,18 +1,21 @@
 import React from 'react';
 import './App.css';
-import Content from './components/Content/Content.jsx';
+// import Content from './components/Content/Content.jsx';
+import Dialogs from './components/Dialogs/Dialogs';
 import Header from './components/Header/Header.jsx';
 import Nav from './components/Nav/Nav.jsx';
 
-
 const App = () => {
-  return (
-    <div className='app-wrapper'>
-      <Header />
-      <Nav />
-      <Content />
-    </div>
-  );
-}
+	return (
+		<div className='app-wrapper'>
+			<Header />
+			<Nav />
+			<div className='app-wrapper-content'>
+				{/* <Content /> */}
+				<Dialogs />
+			</div>
+		</div>
+	);
+};
 
 export default App;
--- src/components/Content/MyPosts/Post/Post.jsx
+++ src/components/Content/MyPosts/Post/Post.jsx
@@ -2,20 +2,22 @@ import React from 'react';
 import s from './Post.module.css';
 
 const Post = (props) => {
-
-    return (
-        <div className={s.item}>
-            <img src="https://robohash.org/5" alt="" />
-            {props.message}
-            <div>
-                <img src="https://e7.pngegg.com/pngimages/1021/749/png-clipart-facebook-like-icon-social-media-marketing-like-button-facebook-social-network-advertising-like-text-hand.png" alt=""/>
-                <span>Like: {props.like}</span>  
-            </div>
-            <div>
-                <span>Dislike:</span>
-            </div>
-        </div>
-    );
-}
+	return (
+		<div className={s.item}>
+			<img src='https://robohash.org/5' alt='' />
+			{props.message}
+			<div>
+				<img
+					src='https://e7.pngegg.com/pngimages/1021/749/png-clipart-facebook-like-icon-social-media-marketing-like-button-facebook-social-network-advertising-like-text-hand.png'
+					alt=''
+				/>
+				<span>Like: {props.like}</span>
+			</div>
+			<div>
+				<span>Dislike: {props.disLike}</span>
+			</div>
+		</div>
+	);
+};
 
 export default Post;
